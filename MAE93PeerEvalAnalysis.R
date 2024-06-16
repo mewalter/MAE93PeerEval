b7727954354951a859c7c3d945a5fb5fa58cd29f
Mark Walter <26728973+mewalter@users.noreply.github.com>
2024-06-16 12:46:41 -0700

    Added DataCols variable Added lines to remove ranking data if professional familiarity is == 1 Added line to show if all fives

--- MAE93PeerEvalAnalysis.R
+++ MAE93PeerEvalAnalysis.R
@@ -20,18 +20,26 @@ file_list <- list.files()
 # delete some files  
 file_list <- file_list[!grepl(paste0("desktop.ini", collapse = "|"), file_list)]
 
+DataCols <- c("X5","X6","X7","X8")
+
 FileData <- read.xlsx(file_list[1],colNames=FALSE)
 nummembers <- length(FileData$X1)-3
 
-i <- 1   # debugging
+i <- 4   # debugging
 AllRankings = list()          # initialize list variable
 for (i in 1:length(file_list)) {
   cat(file_list[i],sep='\n')
   FileData <- read.xlsx(file_list[i],colNames=FALSE)
   FileData.Rankings <- FileData %>% slice(4:(nummembers+3)) %>% select(1:9) %>%   # collect values for members
-    mutate_at(vars(X3,X4,X5,X6,X7,X8),as.numeric) %>% mutate(across(where(is.factor),as.character)) 
+    mutate_at(DataCols,as.numeric) %>% mutate(across(where(is.factor),as.character)) #%>%
+#    mutate(across(DataCols,replace(FileData.Rankings[DataCols],FileData.Rankings$X4==1,NA)))
+# if X4 == 1, then X5, X6, X7, X8 = NA      ... row above does not work ... rows below are ugly but work
+  FileData.Rankings$X5[FileData.Rankings$X4 == 1] <- FileData.Rankings$X6[FileData.Rankings$X4 == 1] <- 
+    FileData.Rankings$X7[FileData.Rankings$X4 == 1] <- FileData.Rankings$X8[FileData.Rankings$X4 == 1] <- NA
   FileData.Rankings$Rater <- gsub("_.*","",file_list[i])              # insert name of the person doing reviewing
   AllRankings[[i]] <- FileData.Rankings                               # add to master file
+  # If average of X5, X6, X7, X8 = 5 AllFive <- TRUE and Print Warning 
+  cat(mean(as.matrix(FileData.Rankings[DataCols]),na.rm=T),sep='\n')
 }   
 FinalRankings <- dplyr::bind_rows(AllRankings)       # turn list into data frame
 FinalRankings <- FinalRankings %>% arrange(desc(X1))   # just to dispay names together also has comments
